00fc1c9f4b98bc7d333876898fb3e733fc371534
zalegajo <117896149+zalegajo@users.noreply.github.com>
2022-11-10 17:51:57 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,3 @@
-alert("test");
-					var url = "https://api.minetools.eu/ping/zalegajo.maxc.pl/27586";
- 
-					$.getJSON(url, function(r) {
-					//data is the JSON string
-					if(r.error){
-					$('#rest').html('Serwer Offline');
-					return false;
-					} 
-					var pl = '';
-					if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
-					$('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Graczy Online:</b> '+r.players.online+pl);
-    
-					});
+console.log("Zalegajo");
+console.log("Created by: github.com/semazurek Website Theme by: github.com/OLIMINATOR");
+
