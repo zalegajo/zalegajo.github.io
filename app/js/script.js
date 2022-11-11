dbf36f9fe7575be9bfdd7045c42d0963ca81d9d6
zalegajo <117896149+zalegajo@users.noreply.github.com>
2022-11-11 12:59:46 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,36 @@
-console.log("Zalegajo");
-console.log("Created by: github.com/semazurek Website Theme by: github.com/OLIMINATOR");
+	
+	function GetIPAdr()
+    {
+	    navigator.clipboard.writeText('zalegajo.maxc.pl');
+    }
+	
+	function HideALL()
+	{
+		const btn = document.getElementById('tohide');
+		btn.style.display = 'none';
+		
+		document.getElementById('adres_serwera').style.display ='block';
+		document.getElementById('aktualnosci').style.display ='block';
+		document.getElementById('pobierz').style.display ='block';
 
+	}
+	
+	function ShowAll()
+	{
+	const btn = document.getElementById('tohide');
+	btn.style.display = 'block';
+	}	
+
+	window.onload = function() 
+	{
+		let date_1 = new Date('04/10/2023');
+		let date_2 = new Date();
+		
+		let difference = date_1.getTime() - date_2.getTime();
+		
+		let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+		console.log(TotalDays + ' dni do końca serwera.');
+		
+		document.getElementById('dni').innerHTML=TotalDays;
+	};
+	
